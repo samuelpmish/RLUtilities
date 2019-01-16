e3b554d2bde815523c5c402b9c3a622cbee64c27
Marvin <35970829+RLMarvin@users.noreply.github.com>
2019-01-16 16:03:18 +0000

    Add .bat build option

---
+++ RLUtilities/build_all.bat
@@ -0,0 +1,18 @@
+SET pypath="C:\Users\Admin\AppData\Local\Programs\Python"
+
+cd cpp
+
+mkdir build_py36
+cd build_py36
+cmake .. -DPYTHON_EXECUTABLE="%pypath%\Python36\python.exe" .. -G"Visual Studio 15 2017 Win64"
+cmake --build . --config Release
+cd ..
+
+
+mkdir build_py37
+cd build_py37
+cmake .. -DPYTHON_EXECUTABLE="%pypath%\Python37\python.exe" .. -G"Visual Studio 15 2017 Win64"
+cmake --build . --config Release
+cd ..
+
+for /R . %%f in (*.pyd) do copy "%%f" ..\..
