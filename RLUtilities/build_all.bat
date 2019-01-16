06f66f73b8efa512b7f8a575a00b38db11826668
Marvin <35970829+RLMarvin@users.noreply.github.com>
2019-01-16 15:35:10 +0000

    Use .bat instead of .sh

---
+++ RLUtilities/build_all.bat
@@ -0,0 +1,32 @@
+git submodule init
+git submodule update --remote
+
+SET pypath="C:\Users\Admin\AppData\Local\Programs\Python"
+
+cd cpp
+
+mkdir build_py36
+cd build_py36
+cmake .. -DPYTHON_EXECUTABLE="%pypath%\Python36\python.exe" .. -G"Visual Studio 15 2017 Win64"
+cmake --build . --config Release
+cd ..
+
+mkdir build_x86_py36
+cd build_x86_py36
+cmake .. -DPYTHON_EXECUTABLE="%pypath%\Python36-32\python.exe" .. -G"Visual Studio 15 2017"
+cmake --build . --config Release
+cd ..
+
+mkdir build_py37
+cd build_py37
+cmake .. -DPYTHON_EXECUTABLE="%pypath%\Python37\python.exe" .. -G"Visual Studio 15 2017 Win64"
+cmake --build . --config Release
+cd ..
+
+mkdir build_x86_py37
+cd build_x86_py37
+cmake .. -DPYTHON_EXECUTABLE="%pypath%\Python37-32\python.exe" .. -G"Visual Studio 15 2017"
+cmake --build . --config Release
+cd ..
+
+for /R . %%f in (*.pyd) do copy "%%f" ..\..\python\RLUtilities
