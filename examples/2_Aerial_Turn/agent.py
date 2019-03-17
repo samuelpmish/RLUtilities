03304928610a57c2ebfbf0f37da1d29469213e00
samuelpmish <samuelpmish@users.noreply.github.com>
2019-03-17 16:11:00 -0700

    fixed Aerial_Turn example

--- examples/2_Aerial_Turn/agent.py
+++ examples/2_Aerial_Turn/agent.py
@@ -5,78 +5,63 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator
 
+from rlutilities.linear_algebra import *
 from rlutilities.mechanics import AerialTurn
-from rlutilities.simulation import GameData, gametype, Car
-from rlutilities.linear_algebra import vec3, mat3, dot, euler_to_rotation
-
+from rlutilities.simulation import Game, Ball, Car
 
 class Agent(BaseAgent):
 
     def __init__(self, name, team, index):
-        gametype("soccar")
-        self.index = index
-        self.info = GameData("TurnMeRightRound", index, team)
+        self.game = Game(index, team)
         self.controls = SimpleControllerState()
 
         self.timer = 0.0
         self.action = None
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        self.info.read_game_tick_packet(packet)
+        self.game.read_game_information(packet,
+                                        self.get_rigid_body_tick(),
+                                        self.get_field_info())
         self.controls = SimpleControllerState()
 
-        if self.action == None:
-            self.action = AerialTurn(self.info.my_car)
-            self.action.target = mat3(1, 0, 0, 0, 1, 0, 0, 0, 1)
+        if not self.action:
+            self.action = AerialTurn(self.game.my_car)
 
-        if self.timer < 0.05:
+        if self.timer == 0.0:
 
-            r = vec3(random.uniform(-1.5, 1.5),
-                     random.uniform(-1.5, 1.5),
-                     random.uniform(-1.5, 1.5))
-
-            omega = vec3(random.uniform(-3.0, 3.0),
-                         random.uniform(-3.0, 3.0),
-                         random.uniform(-3.0, 3.0))
-
-            theta = euler_to_rotation(r)
-
-            self.info.my_car.on_ground = False
-            self.info.my_car.rotation = theta
-            self.info.my_car.angular_velocity = omega
-
-            dt = 1.25 * (self.action.simulate().time - self.info.my_car.time)
-
-            position = Vector3(1000, 1000, 25 + 325 * dt * dt)
-            velocity = Vector3(0, 0, 0)
-            rotation = Rotator(r[0], r[1], r[2])
-            angular_velocity = Vector3(omega[0], omega[1], omega[2])
-
-            car_state = CarState(physics=Physics(
-                location=position,
-                velocity=velocity,
-                rotation=rotation,
-                angular_velocity=angular_velocity
+            # randomly generate a proper orthogonal matrix
+            self.action.target = axis_to_rotation(vec3(
+                random.uniform(-2, 2),
+                random.uniform(-2, 2),
+                random.uniform(-2, 2)
             ))
 
-            self.set_game_state(GameState(cars={self.index: car_state}))
+        f = vec3(self.action.target[0, 0], self.action.target[1, 0], self.action.target[2, 0])
+        position = Vector3(0, 0, 1000)
+        velocity = Vector3(0, 0, 0)
 
-        if self.timer > 0.10:
+        car_state = CarState(physics=Physics(
+            location=position,
+            velocity=velocity
+        ))
 
-            self.action.step(1.0 / 60.0)
-            self.controls = self.action.controls
+        # spawn the ball in front of the desired orientation to visually indicate where we're trying to go
+        ball_state = BallState(physics=Physics(
+            location=Vector3(500 * f[0], 500 * f[1], 500 * f[2] + 1000)
+        ))
 
-            self.timer += 1.0 / 60.0
-            if self.info.my_car.on_ground:
-                print("target:\n", self.action.target)
-                print("theta:\n", self.info.my_car.rotation)
-                print()
-                self.timer = 0.0
-                self.action = None
+        self.set_game_state(GameState(ball=ball_state, cars={self.game.id: car_state}))
 
-        self.info.my_car.last_input.roll = self.controls.roll
-        self.info.my_car.last_input.pitch = self.controls.pitch
-        self.info.my_car.last_input.yaw = self.controls.yaw
+        self.action.step(self.game.time_delta)
+        self.controls = self.action.controls
 
-        self.timer += 1.0 / 60.0
+        self.game.my_car.last_input.roll = self.controls.roll
+        self.game.my_car.last_input.pitch = self.controls.pitch
+        self.game.my_car.last_input.yaw = self.controls.yaw
+
+        self.timer += self.game.time_delta
+        if self.timer > 2.0:
+            self.timer = 0.0
+
         return self.controls
+
